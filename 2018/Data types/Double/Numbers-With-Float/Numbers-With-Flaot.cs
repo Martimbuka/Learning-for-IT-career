55820779d8730d35bc184035a7354d6ef61f5c5d
Martin Jivkov <martin.jivkov5@gmail.com>
2018-02-08 15:21:35 +0200

    Added exercise

---
+++ 2018/Data types/Double/Numbers-With-Float/Numbers-With-Flaot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_With_Float
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+        }
+    }
+}
